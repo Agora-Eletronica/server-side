b4b747d0086b236c92948f5248c41437419f7a99
Samuel Sousa <samuelsousapereira@gmail.com>
2019-09-11 19:14:01 -0300

    Update controller.js
    Adicionando as chaves que estavam faltando

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,11 @@
 const Model = require('../models/model');
 
-module.exports = async function(){
-        const profile = await Model.create({ 
-            usuario,
-            senha
-        })
-        return res.json(profile);
-  
+module.exports = {
+        async function(){
+                const profile = await Model.create({ 
+                        usuario,
+                        senha
+                })
+                return res.json(profile);
+        }
 };
