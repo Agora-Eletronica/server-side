fc16b60cc6945db1bd1f183ceff73f88dfad7b15
Samuel Sousa <samuelsousapereira@gmail.com>
2019-09-11 14:21:53 -0300

    Adicionando campos no model

--- models/model.js
+++ models/model.js
@@ -12,13 +12,64 @@ const ApiSchema = new Schema({
         required: true,
         minlength: 4
     },
+    sexo: {
+        type: Boolean,
+        required: true
+    },
     cpf: {
         type: Number,
         required: true,
+    },
+    usuario: {
+        type: String,
+        required: true
     },
     senha: {
         type: String,
         required: true,
         select: false
+    },
+    nome_do_pai: {
+        type: String,
+        required: false
+    },
+    nome_da_mãe: {
+        type: String,
+        required: true,
+        minlength: 15
+    },
+    nascimento: {
+        type: Date,
+        required: true,
+    },
+    naturalidade: {
+        type: String,
+        required: false,
+    },
+    UF: {
+        type: String,
+        required: false
+    },
+    CEP: {
+        type: Number,
+        required: true
+    },
+    logradouro: {
+        type: String,
+    },
+    número: {
+        type: Number,
+    },
+    complemento: {
+        type: String,
+    },
+    bairro: {
+        type: String,
+    },
+    municipio: {
+        type: String,
+    },
+    UF_nascimento: {
+        type: String,
     }
 })
