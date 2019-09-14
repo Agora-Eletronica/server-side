2cccde7beadcb72cfa10971905357c1b5f1aa01c
luck6o <lucasrobval@gmail.com>
2019-09-14 13:54:18 -0300

    Edições no cadastrocontroller, Criação de update, destroy, index no signup

--- controllers/SignupController.js
+++ controllers/SignupController.js
@@ -1,20 +1,50 @@
 const Model = require('../models/LoginModel');
 
 module.exports = {
+    async index(req, res) {
+
+        usuario = req.params.usuario;
+
+        const usuarioLogado = await Model.findOne({usuario});
+
+        return res.json(usuarioLogado);
+
+    },
+
     async store(req, res){
+        const {
+            usuario,
+            senha
+        } = req.body;
+
+        const usuarioLogado = await Model.findOne({usuario});
+
+        if(usuarioLogado) return res.json({message: "Usuário já em uso"});
+       
         const profile = await Model.create({ 
-            usuario: req.body.usuario,
-            senha: req.body.senha
-        })
+            usuario,
+            senha
+        });
+
+        
+        
         return res.json(profile);
-  
     },
 
     async update(req, res) {
+        const usuarioExiste = await Model.findOne({usuario: req.body.usuario});
 
+        if(!usuarioExiste) return res.json({ message: "Usuário não existe" });
+        
+
+        const senha1 = await Model.findByIdAndUpdate(req.params.id, req.body.senha, { new: true });
+
+        return res.json(senha1);
     },
 
     async destroy(req, res) {
+        await Model.findByIdAndRemove(req.params.id);
 
+        return res.json({ message: "Usuário excluido com sucesso" });
     }
 };
--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const UserController = require('../controllers/SignupController');
 
+router.get('/signup/:id', UserController.index);
 router.post('/signup', UserController.store);
+router.put('/signup/update/:id', UserController.update);
+router.delete('/signup/delete/:id', UserController.destroy);
 
 module.exports = router;
